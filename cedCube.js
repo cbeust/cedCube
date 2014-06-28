84b4155a537277a067dc58c240fbfc582887b7d8
Cedric Beust <cedric@refresh.io>
2014-06-28 12:48:20 -0700

    Cubits working.

--- cedCube.js
+++ cedCube.js
@@ -127,7 +127,7 @@ function addCubeToScene(scene) {
     clearScene();
     faceGroup = new THREE.Object3D();
     var cubitIndex = 0;
-    for (var z = -1; z <= 1; z++) {
+    for (var z = 1; z >= -1; z--) {
         for (var y = 1; y >= -1; y--) {
             for (var x = -1; x <= 1; x++) {
                 var currentFace = facesToRotate.length > 0
@@ -153,23 +153,163 @@ function addCubeToScene(scene) {
     console.log("All objects: " + allObjects.length);
 }
 
-var W = 0xffffff;
-var B = 0x00ff00;
-var Y = 0xffff00;
-var G = 0x00ff00;
-var O = 0xff8c00;
-var R = 0xff0000;
-var X = 0x888888;
+var rgbMap = {
+    'w': 0xffffff,
+    'b': 0x0000ff,
+    'y': 0xffff00,
+    'g': 0x00ff00,
+    'o': 0xff8c00,
+    'r': 0xff0000,
+    '.': 0x888888
+}
+
+var colorString = "wwwwwwwwwbbbbbbbbbyyyyyyyyygggggggggooooooooorrrrrrrrr";
+
+var CUBIT_COLOR_INDICES = [
+    {
+        indices: [ 0, -1, -1, 29, 42, -1 ],
+        expected: "w..go."
+    },
+    {
+        indices: [ 1, -1, -1, -1, 43, -1 ],
+        expected: "w...o."
+    },
+    {
+        indices: [ 2, 9, -1, -1, 44, -1 ],
+        expected: "wb..o."
+    },
+    {
+        indices: [ 3, -1, -1, 32, -1, -1 ],
+        expected: "w..g.."
+    },
+    {
+        indices: [ 4, -1, -1, -1, -1, -1 ], // middle of front
+        expected: "w....."
+    },
+    {
+        indices: [ 5, 12, -1, -1, -1, -1 ],
+        expected: "wb...."
+    },
+    {
+        indices: [ 6, -1, -1, 35, -1, 45 ],
+        expected: "w..g.r"
+    },
+    {
+        indices: [ 7, -1, -1, -1, -1, 46 ],
+        expected: "w....r"
+    },
+    {
+        indices: [ 8, 15, -1, -1, -1, 47 ],
+        expected: "wb...r"
+    },
+    {
+        indices: [ -1, -1, -1, 28, 39, -1 ],
+        expected: "...go."
+    },
+    {
+        indices: [ -1, -1, -1, -1, 40, -1 ], // middle
+        expected: "....o."
+    },
+    {
+        indices: [ -1, 10, -1, -1, 41, -1 ],
+        expected: ".b..o."
+    },
+    {
+        indices: [ -1, -1, -1, 31, -1, -1 ],
+        expected: "...g.."
+    },
+    {
+        indices: [ -1, -1, -1, -1, -1, -1 ], // center cube
+        expected: "......"
+    },
+    {
+        indices: [ -1, 13, -1, -1, -1, -1 ], // middle of right
+        expected: ".b...."
+    },
+    {
+        indices: [ -1, -1, -1, 34, -1, 48 ],
+        expected: "...g.r"
+    },
+    {
+        indices: [ -1, -1, -1, -1, -1, 49 ], // middle of down
+        expected: ".....r"
+    },
+    {
+        indices: [ -1, 16, -1, -1, -1, 50 ],
+        expected: ".b...r"
+    },
+    {
+        indices: [ -1, -1, 20, 27, 36 ],
+        expected: "..ygo"
+    },
+    {
+        indices: [ -1, -1, 19, -1, 37, -1 ],
+        expected: "..y.o."
+    },
+    {
+        indices: [ -1, 11, 18, -1, 38, -1 ],
+        expected: ".by.o."
+    },
+    {
+        indices: [ -1, -1, 23, 30, -1, -1],
+        expected: "..yg.."
+    },
+    {
+        indices: [ -1, -1, 22, -1, -1, -1 ], // middle of back
+        expected: "..y..."
+    },
+    {
+        indices: [ -1, 14, 21, -1, -1, -1 ],
+        expected: ".by..."
+    },
+    {
+        indices: [ -1, -1, 26, 33, -1, -1 ],
+        expected: "..yg.."
+    },
+    {
+        indices: [ -1, -1, 25, -1, -1, 52 ],
+        expected: "..y..r"
+    },
+    {
+        indices: [ -1, 17, 24, -1, -1, 51 ],
+        expected: ".by..r"
+    },
+]
+
+function getCubitColorsFromArray(indices) {
+    var colors = "";
+    for (var i = 0; i < indices.length; i++) {
+        var ind = indices[i];
+        colors += ind != -1 ? colorString[ind] : ".";
+    }
+    return colors;
+//    console.log("Cubit colors: " + colors + " expected: " + expected);
+}
+
+function stringToRgb(s) {
+    var result = [];
+    for (var i = 0; i < s.length; i++) {
+        result.push(rgbMap[s.charAt(i)])
+    }
+    return result;
+}
 
 function getMaterialArray(cubitIndex) {
     var array = [];
+    var ci = CUBIT_COLOR_INDICES[cubitIndex].indices;
+    var cs = getCubitColorsFromArray(ci);
+    console.log("Color of cubit " + cubitIndex + " " + cs);
+    var rgbColors = stringToRgb(cs);
+
+    // input order: z+,x+,z-,x-,y+,y-
     // order to add materials: x+,x-,y+,y-,z+,z-
-    array.push( new THREE.MeshBasicMaterial( { color: X } ) );
-    array.push( new THREE.MeshBasicMaterial( { color: G } ) );
-    array.push( new THREE.MeshBasicMaterial( { color: O } ) );
-    array.push( new THREE.MeshBasicMaterial( { color: X } ) );
-    array.push( new THREE.MeshBasicMaterial( { color: W } ) );
-    array.push( new THREE.MeshBasicMaterial( { color: X } ) );
+    array.push( new THREE.MeshBasicMaterial( { color: rgbColors[1] } ) );
+    array.push( new THREE.MeshBasicMaterial( { color: rgbColors[3] } ) );
+    array.push( new THREE.MeshBasicMaterial( { color: rgbColors[4] } ) );
+    array.push( new THREE.MeshBasicMaterial( { color: rgbColors[5] } ) );
+    array.push( new THREE.MeshBasicMaterial( { color: rgbColors[0] } ) );
+    array.push( new THREE.MeshBasicMaterial( { color: rgbColors[2] } ) );
+
     return new THREE.MeshFaceMaterial( array );
 }
 
@@ -246,14 +386,35 @@ document.onkeydown = function() {
 
 };
 
+function testCubitColors() {
+    for (var i = 0; i < CUBIT_COLOR_INDICES.length; i++) {
+        var oi = CUBIT_COLOR_INDICES[i];
+        var colors = getCubitColorsFromArray(oi.indices);
+        if (colors != oi.expected) {
+            console.log("*** Mismatch index " + i + ": " + colors + " expected: " + oi.expected);
+        }
+    }
+
+    for (var i = 0; i < 27; i++) {
+        getMaterialArray(i);
+    }
+
+    console.log(CUBIT_COLOR_INDICES.length + " tests done");
+}
+
+testCubitColors();
+
 function runCube() {
+    camera.position.x = 150;
+    camera.position.y = 200;
     camera.position.z = 500;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild( renderer.domElement );
 
     addCubeToScene(scene, null);
-    rotateCube(FRONT);
+//    rotateCube(FRONT);
 //    rotateCube(UP);
 //    rotateCube(RIGHT);
 //    rotateCube(FRONT_PRIME);
