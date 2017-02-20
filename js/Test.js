a2519061958cba44d6e3346687056443225b80ce
Cedric Beust <cedric.beust@le.com>
2017-02-20 10:15:22 -0800

    Working transparent cube.

--- js/Test.js
+++ js/Test.js
@@ -2,14 +2,14 @@
  * Created by cedricbeust on 2/19/17.
  */
 var camera, scene, renderer, geometry, mesh, controls;
-
 var group;
+var cubitSize = 90;
 
 init();
 animate();
 
 function createPlane(x, y, z, color) {
-    var geometry = new THREE.PlaneGeometry( 100, 100 );
+    var geometry = new THREE.PlaneGeometry( cubitSize, cubitSize );
     var material = new THREE.MeshBasicMaterial({
         color: color,
         side: THREE.DoubleSide
@@ -22,34 +22,33 @@ function createPlane(x, y, z, color) {
     return result;
 }
 
-function createCube(config) {
+function createCubit(config) {
     group = new THREE.Object3D();
 
-    var pos = 100;
 
     if (config.left) {
-        var left = createPlane(-pos / 2, 0, pos / 2, config.left);
+        var left = createPlane(-cubitSize / 2, 0, 0, config.left);
         left.rotation.y = Math.PI / 2;
         group.add(left);
     }
     if (config.right) {
-        var right = createPlane(pos / 2, 0, pos / 2, config.right);
+        var right = createPlane(cubitSize / 2, 0, 0, config.right);
         right.rotation.y = Math.PI / 2;
         group.add(right);
     }
     if (config.back) {
-        group.add(createPlane(0, 0, 0, config.back));
+        group.add(createPlane(0, 0, -cubitSize/2, config.back));
     }
     if (config.front) {
-        group.add(createPlane(0, 0, pos, config.front));
+        group.add(createPlane(0, 0, cubitSize/2, config.front));
     }
     if (config.bottom) {
-        var front = createPlane(0, -pos / 2, pos / 2, config.bottom);
+        var front = createPlane(0, -cubitSize / 2, 0, config.bottom);
         front.rotation.x = Math.PI / 2;
         group.add(front);
     }
     if (config.top) {
-        var ttop = createPlane(0, pos / 2, pos / 2, config.top);
+        var ttop = createPlane(0, cubitSize / 2, 0, config.top);
         ttop.rotation.x = Math.PI / 2;
         group.add(ttop);
     }
@@ -62,28 +61,87 @@ function init() {
     scene = new THREE.Scene();
 
     // camera
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight);
     // camera.position.set(100, -800, 400);
     // camera.rotation.x = 45 * ( Math.PI / 180 );
     // camera.position.x = 200;
-    camera.position.z = 800;
+    camera.position.x = 600;
+    camera.position.y = 600;
+    camera.position.z = 1500;
     // camera.position.set(100, -400, 100);
     scene.add(camera);
 
-    scene.add(new THREE.AxisHelper(100));
+    // The X axis is red. The Y axis is green. The Z axis is blue.
+    scene.add(new THREE.AxisHelper(500));
 
-    scene.add(createCube({
-        bottom: "blue",
-        top: "green",
-        left: "red",
-        right: "orange",
-        back: "yellow",
-        front: "white"
-    }));
+    // var s = "..gggggggooooooooobbbbbbbbbrrrrrrrrryyyyyyyyywwwwwwwww";
+    var s = "....bbrb....rrr.rrXXXggggggXXXooXoob.........wwXwwwwww";
 
-    // plane2 = createPlane();
-    // plane2.position.x += 200;
-    // scene.add(plane2);
+    var mapping = [
+        { x: -1, y: 1, z: 1, front: 0, top: 42, left: 29 },
+        { x: 0, y: 1, z: 1, front: 1, top: 43 },
+        { x: 1, y: 1, z: 1, front: 2, top: 44, right: 9 },
+
+        { x: -1, y: 0, z: 1, front: 3, left: 32 },
+        { x: 0, y: 0, z: 1, front: 4 },
+        { x: 1, y: 0, z: 1, front: 5, right: 12 },
+
+        { x: -1, y: -1, z: 1, front: 6, bottom: 45, left: 35 },
+        { x: 0, y: -1, z: 1, front: 7, bottom: 46 },
+        { x: 1, y: -1, z: 1, front: 8, bottom: 47, right: 15 },
+
+        { x: -1, y: 1, z: 0, left: 28, top: 39 },
+        { x: 0, y: 1, z: 0, top: 40 },
+        { x: 1, y: 1, z: 0, right: 10, top: 41 },
+
+        { x: -1, y: 0, z: 0, left: 31 },
+        { x: 1, y: 0, z: 0, right: 13 },
+
+        { x: -1, y: -1, z: 0, left: 34, bottom: 48 },
+        { x: 0, y: -1, z: 0, bottom: 49 },
+        { x: 1, y: -1, z: 0, right: 16, bottom: 50 },
+
+        { x: -1, y: 1, z: -1, top: 36, left: 27, back: 20 },
+        { x: 0, y: 1, z: -1, top: 37, back: 19 },
+        { x: 1, y: 1, z: -1, top: 38, right: 11, back: 18 },
+
+        { x: -1, y: 0, z: -1, left: 30, back: 23 },
+        { x: 0, y: 0, z: -1, back: 22 },
+        { x: 1, y: 0, z: -1, right: 14, back: 21 },
+
+        { x: -1, y: -1, z: -1, left: 33, back: 26, bottom: 51 },
+        { x: 0, y: -1, z: -1, back: 25, bottom: 51 },
+        { x: 1, y: -1, z: -1, right: 17, back: 24, bottom: 52 }
+
+    ];
+    var colorMapping = {
+        "g": "green",
+        "b": "blue",
+        "r": "red",
+        "o": "orange",
+        "w": "white",
+        "y": "yellow",
+        "X": "grey"
+    };
+
+    var SIDES = [ "front", "right", "back", "left", "top", "bottom"];
+    for (var i = 0; i < mapping.length; i++) {
+        var m = mapping[i];
+        var config = {};
+        for (var j = 0; j < SIDES.length; j++) {
+            if (m[SIDES[j]] != null) {
+                var colorIndex = m[SIDES[j]];
+                var colorChar = s.charAt(colorIndex);
+                config[SIDES[j]] = colorMapping[colorChar];
+            }
+        }
+        console.log("M: " + m);
+        var cubit = createCubit(config);
+        cubit.position.x += 100 * m.x;
+        cubit.position.y += 100 * m.y;
+        cubit.position.z += 100 * m.z;
+        scene.add(cubit);
+    }
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -93,15 +151,12 @@ function init() {
 }
 
 function animate() {
-
     requestAnimationFrame(animate);
     render();
-
 }
 
 
 function render() {
-
     // mesh.rotation.x += 0.01;
     // mesh.rotation.y += 0.02;
 
@@ -111,5 +166,4 @@ function render() {
     controls.update();
 
     renderer.render(scene, camera);
-
 }
--- js/TransparentCube.js
+++ js/TransparentCube.js
@@ -1,127 +1,169 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 10000);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+/**
+ * Created by cedricbeust on 2/19/17.
+ */
+var camera, scene, renderer, geometry, mesh, controls;
+var group;
+var cubitSize = 90;
 
-function createMaterial(c, o) {
-    return new THREE.MeshBasicMaterial({ color: c, transparent: true, opacity: o, side: THREE.DoubleSide
-        // , color: THREE.FaceColors
-    })
-}
+init();
+animate();
 
-var white = createMaterial(0xf5f5f5, 0.8);
-var blue = createMaterial(0x0026c4, 0.8);
-var yellow = createMaterial(0xf9d720, 0.8);
-var green = createMaterial(0x77cb3e, 0.8);
-var orange = createMaterial(0xf57c08, 0.8);
-var red = createMaterial(0xd62828, 0.8);
-var transparent = new THREE.ShadowMaterial({ color: 0xffffff, transparent: true, opacity: 0.1, side: THREE.DoubleSide})
-
-function createCube(colors) { // front, right, back, left, top, bottom) {
-    var right = colors.right ? colors.right : transparent;
-    var left = colors.left ? colors.left : transparent;
-    var top = colors.top ? colors.top : transparent;
-    var bottom = colors.bottom ? colors.bottom : transparent;
-    var front = colors.front ? colors.front : transparent;
-    var back = colors.back ? colors.back : transparent;
-
-    var materials = new THREE.MultiMaterial([right, left, top, bottom, front, back]);
-
-    for (var i = -1; i <= 1; i++) {
-        for (var j = -1; j <= 1; j++) {
-            for (var k = -1; k <= 1; k++) {
-                console("Creating plane at " + i + " " + j + " " + k);
-                var planeGeometry = new THREE.PlaneBufferGeometry(1000, 1000);
-                var planeMaterials = new THREE.MeshPhongMaterial({
-                    color: 'blue',
-                    side: THREE.DoubleSide
-                });
-                var planeMesh = new THREE.Mesh(planeGeometry, planeMaterials);
-                planeMesh.position.x = 1000*i;
-                planeMesh.position.y = 1000*j;
-                planeMesh.position.z = 1000*k;
-            }
-        }
-    }
-
-    materials.side = THREE.DoubleSide;
-    var planeGeometry = new THREE.PlaneBufferGeometry(1000, 1000, 10, 10);
-    var planeMaterials = new THREE.MeshPhongMaterial({
-        color: 'blue',
+function createPlane(x, y, z, color) {
+    var geometry = new THREE.PlaneGeometry( cubitSize, cubitSize );
+    var material = new THREE.MeshBasicMaterial({
+        color: color,
         side: THREE.DoubleSide
     });
-    // var geometry = new THREE.BoxGeometry(1000, 1000, 1000);
-    var cube = new THREE.Mesh(planeGeometry, planeMaterials);
-    // var cube = new THREE.Mesh(geometry, materials);
-    cube.position = new THREE.Vector3(0, 0, 0);
+    var result = new THREE.Mesh( geometry, material );
+    result.position.x = x;
+    result.position.y = y;
+    result.position.z = z;
 
-    return cube;
+    return result;
 }
 
-/**
- * Example string:  "g..r.w" to produce a green(front), red(left), white(bottom).
- * @param string
- */
-function createColorOption(string) {
-    var colors = {}
+function createCubit(config) {
+    group = new THREE.Object3D();
 
-    function charToColor(ch) {
-        if (ch == 'w') return white;
-        else if (ch == 'b') return blue;
-        else if (ch == 'y') return yellow;
-        else if (ch == 'g') return green;
-        else if (ch == 'o') return orange;
-        else if (ch == 'r') return red;
+
+    if (config.left) {
+        var left = createPlane(-cubitSize / 2, 0, 0, config.left);
+        left.rotation.y = Math.PI / 2;
+        group.add(left);
+    }
+    if (config.right) {
+        var right = createPlane(cubitSize / 2, 0, 0, config.right);
+        right.rotation.y = Math.PI / 2;
+        group.add(right);
+    }
+    if (config.back) {
+        group.add(createPlane(0, 0, -cubitSize/2, config.back));
+    }
+    if (config.front) {
+        group.add(createPlane(0, 0, cubitSize/2, config.front));
+    }
+    if (config.bottom) {
+        var front = createPlane(0, -cubitSize / 2, 0, config.bottom);
+        front.rotation.x = Math.PI / 2;
+        group.add(front);
+    }
+    if (config.top) {
+        var ttop = createPlane(0, cubitSize / 2, 0, config.top);
+        ttop.rotation.x = Math.PI / 2;
+        group.add(ttop);
     }
 
-    var c = string.charAt(0);
-    if (c != ".") colors.front = charToColor(c);
-    c = string.charAt(1);
-    if (c != ".") colors.right = charToColor(c);
-    c = string.charAt(2);
-    if (c != ".") colors.back = charToColor(c);
-    c = string.charAt(3);
-    if (c != ".") colors.left = charToColor(c);
-    c = string.charAt(4);
-    if (c != ".") colors.top = charToColor(c);
-    c = string.charAt(5);
-    if (c != ".") colors.bottom = charToColor(c);
-
-    return colors;
+    return group;
 }
 
-// var option = createColorOption("g..r.w");
-// if (option.front == null || option.left == null || option.bottom == null
-//     || option.back != null || option.right != null || option.top != null) {
-//     error("Test failed");
-// }
+function init() {
 
-var cube = createCube({ front: white, back: red })
-scene.add(cube);
+    scene = new THREE.Scene();
 
+    // camera
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight);
+    // camera.position.set(100, -800, 400);
+    // camera.rotation.x = 45 * ( Math.PI / 180 );
+    // camera.position.x = 200;
+    camera.position.x = 600;
+    camera.position.y = 600;
+    camera.position.z = 1500;
+    // camera.position.set(100, -400, 100);
+    scene.add(camera);
 
-var light = new THREE.AmbientLight(0xffffff, 10);
-scene.add(light);
+    // The X axis is red. The Y axis is green. The Z axis is blue.
+    scene.add(new THREE.AxisHelper(500));
 
-camera.position.x = 1000;
-camera.position.y = 1000;
-camera.position.z = 1000;
+    // var s = "..gggggggooooooooobbbbbbbbbrrrrrrrrryyyyyyyyywwwwwwwww";
+    var s = "....bbrb....rrr.rrXXXggggggXXXooXoob.........wwXwwwwww";
 
-var cubeCenter = new THREE.Vector3(10, 10, 10)
+    var mapping = [
+        { x: -1, y: 1, z: 1, front: 0, top: 42, left: 29 },
+        { x: 0, y: 1, z: 1, front: 1, top: 43 },
+        { x: 1, y: 1, z: 1, front: 2, top: 44, right: 9 },
 
-var controls = new THREE.OrbitControls(camera, renderer.domElement)
+        { x: -1, y: 0, z: 1, front: 3, left: 32 },
+        { x: 0, y: 0, z: 1, front: 4 },
+        { x: 1, y: 0, z: 1, front: 5, right: 12 },
+
+        { x: -1, y: -1, z: 1, front: 6, bottom: 45, left: 35 },
+        { x: 0, y: -1, z: 1, front: 7, bottom: 46 },
+        { x: 1, y: -1, z: 1, front: 8, bottom: 47, right: 15 },
+
+        { x: -1, y: 1, z: 0, left: 28, top: 39 },
+        { x: 0, y: 1, z: 0, top: 40 },
+        { x: 1, y: 1, z: 0, right: 10, top: 41 },
+
+        { x: -1, y: 0, z: 0, left: 31 },
+        { x: 1, y: 0, z: 0, right: 13 },
+
+        { x: -1, y: -1, z: 0, left: 34, bottom: 48 },
+        { x: 0, y: -1, z: 0, bottom: 49 },
+        { x: 1, y: -1, z: 0, right: 16, bottom: 50 },
+
+        { x: -1, y: 1, z: -1, top: 36, left: 27, back: 20 },
+        { x: 0, y: 1, z: -1, top: 37, back: 19 },
+        { x: 1, y: 1, z: -1, top: 38, right: 11, back: 18 },
+
+        { x: -1, y: 0, z: -1, left: 30, back: 23 },
+        { x: 0, y: 0, z: -1, back: 22 },
+        { x: 1, y: 0, z: -1, right: 14, back: 21 },
+
+        { x: -1, y: -1, z: -1, left: 33, back: 26, bottom: 51 },
+        { x: 0, y: -1, z: -1, back: 25, bottom: 51 },
+        { x: 1, y: -1, z: -1, right: 17, back: 24, bottom: 52 }
+
+    ];
+    var colorMapping = {
+        "g": "green",
+        "b": "blue",
+        "r": "red",
+        "o": "orange",
+        "w": "white",
+        "y": "yellow",
+        "X": "grey"
+    };
+
+    var SIDES = [ "front", "right", "back", "left", "top", "bottom"];
+    for (var i = 0; i < mapping.length; i++) {
+        var m = mapping[i];
+        var config = {};
+        for (var j = 0; j < SIDES.length; j++) {
+            if (m[SIDES[j]] != null) {
+                var colorIndex = m[SIDES[j]];
+                var colorChar = s.charAt(colorIndex);
+                config[SIDES[j]] = colorMapping[colorChar];
+            }
+        }
+        console.log("M: " + m);
+        var cubit = createCubit(config);
+        cubit.position.x += 100 * m.x;
+        cubit.position.y += 100 * m.y;
+        cubit.position.z += 100 * m.z;
+        scene.add(cubit);
+    }
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
 
 function render() {
-    requestAnimationFrame(render);
-    // camera.position.y -= 10;
-    // cube.position.x -= 10;
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+    // mesh.rotation.x += 0.01;
+    // mesh.rotation.y += 0.02;
 
-    camera.lookAt(cubeCenter)
-    // cube.rotation.y += 0.01;
+    // group.rotation.x -= 0.05;
+    // group.rotation.y -= 0.05;
+    // camera.rotation.x += 0.2;
     controls.update();
+
     renderer.render(scene, camera);
-};
-render();
+}
