89feba15461627416872e5dada36187b762ba99f
Cedric Beust <cedric.beust@le.com>
2017-02-28 13:37:57 -0800

    Move things around.

--- js/f2l.js
+++ js/f2l.js
@@ -181,7 +181,275 @@ var section2 = {
     ]
 };
 
-var formulaMap = { "sections": [ section0, section1, section2 ] };
+var section3 = {
+    header: "Corner in wrong slot, edge in the U layer",
+    algorithms: [
+        {
+            id: "ew-cu-00",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttbrtttrrtrrttwbbttbttwtwwwwwwtttooroottttgggggg",
+            formulas: ["L F2 L' F2"]
+        },
+        {
+            id: "ew-cu-01",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttwttttttrrtrrbttbbttbttwtwwwwwwttrooroottttgggggg",
+            formulas: ["F2 R' F2 R", "y F2 L F2 L'"]
+        },
+        {
+            id: "ew-cu-02",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttrwtttrrtrrttbbbttbttwtwwwwwwtttooroottttgggggg",
+            formulas: ["U' (L' U' L) (R U' R')"]
+        },
+        {
+            id: "ew-cu-03",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttbrtttrrtrrttwrbtbbttwwwwwwwwtttooboootttgggggg",
+            formulas: ["L' F' U' F L"]
+        },
+        {
+            id: "ew-cu-04",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttwbtttrrtrrttrrbttbttwtwwwwwwtttooboottttgggggg",
+            formulas: ["L F' L2 U L U2 F", "y R' F R2 U' R' U2 F'"]
+        },
+        {
+            id: "ew-cu-05",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttrwtttrrtrrttbrbttbttwtwwwwwwtttooboottttgggggg",
+            formulas: ["(F U2 F') (R U R')", "y (F' U2 F)(L' U' L)"]
+        }
+    ]
+};
+
+var section4 = {
+    header: "Both corners and edge in wrong slot",
+    algorithms: [
+        {
+            id: "ew-cw-00",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttbbtrbtwwtwwwwwwtttooroobtttgggggg",
+            formulas: ["y' R U' R U2 R2 U2 R U R", "y' R U R U2 R2 U2 R U' R"]
+        },
+        {
+            id: "ew-cw-01",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttttttorrbrrttttbbtbotwwwwwwwwtttootoottttgggggg",
+            formulas: ["y' R' U R' U2 R2 U2 R' U' R'", "y' R' U' R' U2 R2 U2 R' U R'"]
+        },
+        {
+            id: "ew-cw-02",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttbbtbbtrwtwwwwwwtttooroowtttgggggg",
+            formulas: ["y' R U' R2 U' R U' R' U' R", "y' R U' R' U' R U' R2 U' R", "F' R2 U F' R2 F U' R2"]
+        },
+        {
+            id: "ew-cw-03",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttttttorrwrrttttbbtbbtwrwwwwwwtttootoottttgggggg",
+            formulas: ["y' R' U R2 U R' U R U R'", "y' R' U R U' R U' R' U' R U R'", "F L2 U' F L2 F' U L2"]
+        },
+        {
+            id: "ew-cw-04",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttbbtwbtbwtwwwwwwtttooroortttgggggg",
+            formulas: ["L' U L U R U' R' U' R U R'", "y' R U R' U' R U' R' U R' U' R", "y' R U R2 U2 R' U R' U' R2"]
+        },
+        {
+            id: "ew-cw-05",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttttttorrorrttttbbtbwtwbwwwwwwtttootoottttgggggg",
+            formulas: ["R U' R' U' L' U L U L' U' L", "y' R' U' R U R' U R U' R U R'"]
+        },
+        {
+            id: "ew-cw-06",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttrbtrbtwbtwwwwwwtttooboobtttgggggg",
+            formulas: ["L F' L' F R U' R'"]
+        },
+        {
+            id: "ew-cw-07",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttttttbrrbrrttttbotbotwwwwwwwwtttootoottttgggggg",
+            formulas: ["R U R' F' L F L'"]
+        },
+        {
+            id: "ew-cw-08",
+            src: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttrbtbbtrwtwwwwwwtttooboowtttgggggg",
+            formulas: ["y' R U' R2 y R' F' R F"]
+        },
+        {
+            id: "ew-cw-09",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttttttbrrwrrtttttotbbwwowwwwwwtttootoottttgggggg",
+            formulas: ["y' R' U R2 F R F' R'", "y' R' B' R2 U R'", "R U l' L U' R' U' M F", "F' R U' M' U M U R' F"]
+        },
+        {
+            id: "ew-cw-10",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttrbtwbtbwtwwwwwwtttooboortttgggggg",
+            formulas: ["L' U' L y' R' U2 R U R' U' R"]
+        },
+        {
+            id: "ew-cw-11",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttttttbrrorrttttbotbwtwbwwwwwwtttootoottttgggggg",
+            formulas: ["R U R' y' R U2 R' U' R U R'"]
+        }
+    ]
+};
+
+var section5 = {
+    header: "Some diagonal Jeff2L",
+    algorithms: [
+        {
+            id: "diag-00",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttttttrrtrrwtttgbbtbbtwwwwwwwgtttoorootttttggrgg",
+            formulas: ["R' U R' u' R2 u R2"]
+        },
+        {
+            id: "diag-01",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttttttrrtrrrtttrbbtbbtwwwwwwwwtttoogoottttggtggg",
+            formulas: ["R2 F R U' F' R"]
+        },
+        {
+            id: "diag-02",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tgtttttttttttrrwrrtttbbbbbowwgwwwtwwttttootoototggtggt",
+            formulas: ["R2 L U' L' U R2"]
+        },
+        {
+            id: "diag-03",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttgtttttttttrrgrrtttbbtbbwwwowwwtwwtottootootttggtggt",
+            formulas: ["R2 U' L U L' R2"]
+        }
+    ]
+};
+
+var section6 = {
+    header: "Diagonal Jeff2L, edge in BL",
+    algorithms: [
+        {
+            id: "diag-ebl-00",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttwbtttrrtrrttrbbtbbtwwtwwwtwwtttrootootttggbggt",
+            formulas: ["R2 u R2 u' R2"]
+        },
+        {
+            id: "diag-ebl-01",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttbrtttrrtrrttwbbtbbtwwtwwwwwttttrootootttggbggt",
+            formulas: ["U (L U' L') (R U' R')"]
+        },
+        {
+            id: "diag-ebl-02",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttrwtttrrtrrttbbbtbbtwwtwwwwwttttrootootttggbggt",
+            formulas: ["U' (L U L') (R U R')"]
+        },
+        {
+            id: "diag-ebl-03",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttbrtttrrtrrttwbbtbbtwwtwwwtwwtttbootootttggrggt",
+            formulas: ["L F' U' F L'"]
+        },
+        {
+            id: "diag-ebl-04",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttwbtttrrtrrttrbbtbbtwwtwwwwwttttbootootttggrggt",
+            formulas: ["U2 F' (L U' L') F"]
+        },
+        {
+            id: "diag-ebl-05",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttrwtttrrtrrttbbbtbbtwwtwwwwwttttbootootttggrggt",
+            formulas: ["B' (R U R') B"]
+        },
+        {
+            id: "diag-ebl-06",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrrrrtttbbtbbbwwwwwwtwwtttboooootttggrggt",
+            formulas: ["R2 B' R' U B R'"]
+        },
+        {
+            id: "diag-ebl-07",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrbrrtttbbtbbwwwrwwwwwttttrootootttggbggt",
+            formulas: ["(F' U F') D' L2 D F2"]
+        },
+        {
+            id: "diag-ebl-08",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrbrrtttbbtbbwwwrwwwwwttttbootootttggrggt",
+            formulas: ["R U2 B' U B R'"]
+        },
+        {
+            id: "diag-ebl-09",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrwrrtttbbtbbrwwbwwwwwttttrootootttggbggt",
+            formulas: ["(L U L') R U2 (L U' L') R'", "RU'R u R2 u' R2"]
+        },
+        {
+            id: "diag-ebl-10",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrwrrtttbbtbbrwwbwwwwwttttbootootttggrggt",
+            formulas: ["F' U2 (L U' L') F"]
+        }
+    ]
+};
+
+var section7 = {
+    header: "Diagonal Jeff2L, corner in BLD",
+    algorithms: [
+        {
+            id: "diag-cbld-00",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttrtttttrrtrrtbtbbtbbtwwtwwwwwwttttoorootttggtggb",
+            formulas: ["U (L U2 L') (F' U' F)"]
+        },
+        {
+            id: "diag-cbld-01",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttbttttrttrrtrrtttbbtbbtwwtwwwwwwttttoorootttggtggb",
+            formulas: ["U' (B' U2 B) (R U R')"]
+        },
+        {
+            id: "diag-cbld-02",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttrtttttrrtrrtbtbbtbbtwwtwwwwwwttttoowootttggtggr",
+            formulas: ["U2' (L U L') (F' U F)"]
+        },
+        {
+            id: "diag-cbld-03",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttbttttrttrrtrrtttbbtbbtwwtwwwbwwttttoowootttggtggr",
+            formulas: ["U (L U2' L') (R U R')"]
+        }
+    ]
+};
+
+var section8 = {
+    header: "Both pieces in the wrong slot",
+    algorithms: [
+        {
+            id: "both-ws-00",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttbbtbbtwwtwwwwwwtttroorootttggbggb",
+            formulas: ["L R U2 L' R'"]
+        },
+        {
+            id: "both-ws-01",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttbbtbbtwwtwwwwwwtttboorootttggrggb",
+            formulas: ["L U2 F U2 F2 U' F L'"]
+        },
+        {
+            id: "both-ws-02",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttbbtbbtwwtwwwrwwtttroobootttggbggw",
+            formulas: ["L2 R U2 L' U' L U' L2 R'"]
+        },
+        {
+            id: "both-ws-03",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttbbrbbrwwtwwwrwwtttboobootttggrggw",
+            formulas: ["B' U B F' U' F R U R'"]
+        },
+        {
+            id: "both-ws-04",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttbbtbbtwwtwwwwwbtttroowootttggbggr",
+            formulas: ["L U2 F U' F' L' R U R'"]
+        },
+        {
+            id: "both-ws-05",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttttttttrrtrrtttbbtbbtwwtwwwwwbtttboowootttggrggr",
+            formulas: ["L U' L' R U R' F' U' F"]
+        }
+    ]
+};
+
+var section9 = {
+    header: "Intuitive F2L variations",
+    algorithms: [
+        {
+            id: "int-fl2-00",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=ttttttttttttbrrrrrtttbbrbbbwwwwwwwwwtttooooootttgggggg",
+            formulas: ["R U' R' U (R' F R F') (R' F R F') R U' R'", "R' F R F' R U' R' U R U' R' U2 R U' R'"]
+        },
+        {
+            id: "int-fl2-01",
+            url: "http://cube.crider.co.uk/visualcube.php?fmt=svg&size=140&r=y30x-30&cc=n&co=15&fo=35&bg=888888&fo=100&fc=tttttttbwbtttrrtrrtrrbbtbbtwwtwwwwwwtttooooootttgggggg",
+            formulas: ["U2 R2 U2 R' U' R U' R2"]
+        }
+    ]
+};
+
+var formulaMap = { "sections": [ section0, section1, section2, section3, section4, section5, section6, section7,
+    section8, section9 ] };
 var template = "{{#sections}}<div class='section'><h3>{{header}}</h3>{{#algorithms}} " +
     "<div class='cube1' style='float:left'><img src='pics/{{id}}.svg'/><ul>{{#formulas}}<li>{{.}}</li>{{/formulas}}</ul></div>" +
     "{{/algorithms}}</div>{{/sections}}";
