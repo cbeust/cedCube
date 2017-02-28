ff9741d40514fbebbe540a0f4887f5c19c746970
Cedric Beust <cedric@beust.com>
2017-02-27 21:19:26 -0800

    More f2l

---
+++ WebContent/F2L_files/visualcube(58).php
@@ -0,0 +1,3 @@
+<br />
+<b>Warning</b>:  mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: User solids00_algdb already has more than 'max_user_connections' active connections in <b>/home/solids00/domains/solidsites.co.uk/public_html/cube/visualcube.php</b> on line <b>420</b><br />
+Unable to select database
